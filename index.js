044736e966727fb97d4fa9294dca9216c0c1bb5d
Daniel Trone <royhobbstn@yahoo.com>
2017-11-29 01:58:32 +0000

    split lambdas

---
+++ index.js
@@ -0,0 +1,20 @@
+const serverless = require('serverless-http');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.json());
+
+require("./routes/routes.js")(app);
+
+var server = app.listen(8080, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
+
+module.exports.handler = serverless(app);
