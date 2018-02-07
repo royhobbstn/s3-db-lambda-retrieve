0e03efafb9b4845b6479509d453bd71b8827b931
Daniel Trone <danieljtrone@gmail.com>
2018-02-07 11:29:53 +0000

    simplify

--- routes/routes.js
+++ routes/routes.js
@@ -2,18 +2,14 @@
 
 const Parser = require('expr-eval').Parser;
 const present = require('present');
-const zlib = require('zlib');
-const https = require('https');
-
+const rp = require('request-promise');
 
 
 const appRouter = function(app) {
 
-
     app.get("/test", function(req, res) {
         return res.send('test');
     });
-
 
     // https://d0ahqlmxvi.execute-api.us-west-2.amazonaws.com/dev
     // /retrieve?path=e002/140/89&expression=%5B%22B01001001%22%5D&dataset=acs1216
@@ -43,49 +39,43 @@ const appRouter = function(app) {
 
         const url = getUrlFromDataset(dataset);
 
-        const gunzip = zlib.createGunzip();
+        rp({
+            method: 'get',
+            uri: `https://${url}/${path}.json`,
+            headers: {
+                'Accept-Encoding': 'gzip',
+            },
+            gzip: true,
+            json: true,
+            fullResponse: false
+        }).then(data => {
+            //
+            console.log({ time: getTime(start_time), msg: 'received and parsed s3 data' });
 
+            const evaluated = {};
 
-        https.get(`https://${url}/${path}.json`, function(response) {
+            Object.keys(data).forEach((key, i) => {
 
-            let d = "";
-
-            response.pipe(gunzip)
-                .on('data', function(data) {
-                    d += data.toString();
-                })
-                .on('end', function() {
-
-                    console.log({ time: getTime(start_time), msg: 'received s3 data' });
-
-                    const data = JSON.parse(d);
-
-                    console.log({ time: getTime(start_time), msg: 'parsed s3 data' });
-
-                    const evaluated = {};
-
-                    Object.keys(data).forEach((key, i) => {
-
-                        const obj = {};
-                        fields.forEach(field => {
-                            obj[field] = data[key][field];
-                        });
-
-                        if (est_or_moe === 'e') {
-                            evaluated[key] = expr.evaluate(obj);
-                        }
-                        else {
-                            evaluated[`${key}_moe`] = expr.evaluate(obj);
-                        }
-
-                    });
-
-                    console.log({ time: getTime(start_time), msg: 'sending data' });
-
-                    res.json(evaluated);
-
+                const obj = {};
+                fields.forEach(field => {
+                    obj[field] = data[key][field];
                 });
 
+                if (est_or_moe === 'e') {
+                    evaluated[key] = expr.evaluate(obj);
+                }
+                else {
+                    evaluated[`${key}_moe`] = expr.evaluate(obj);
+                }
+
+            });
+
+            console.log({ time: getTime(start_time), msg: 'sending data' });
+
+            return res.json(evaluated);
+            //
+        }).catch(err => {
+            return res.status(500).json({ err });
         });
 
     });
