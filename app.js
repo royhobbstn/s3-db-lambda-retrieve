f857c4cdfe66b819e0ac10d397b55610a165d3c1
Daniel Trone <danieljtrone@gmail.com>
2018-02-25 13:32:29 +0000

    added compression

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const serverless = require('serverless-http');
+const compression = require('compression');
 const express = require("express");
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+// app.use(compression());
 
 
 require("./routes/routes.js")(app);
 
 const server = app.listen(8080, function() {
-    console.log("Listening on port %s...", server.address().port);
+  console.log("Listening on port %s...", server.address().port);
 });
 
 module.exports.handler = serverless(app);
