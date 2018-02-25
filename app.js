831fc3d148215fbc67065f59f98f32d31fb689ba
Daniel Trone <danieljtrone@gmail.com>
2018-02-25 17:26:11 +0000

    i finally understand all my gzip problems

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const serverless = require('serverless-http');
-const compression = require('compression');
 const express = require("express");
 const app = express();
 
@@ -8,8 +7,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-// app.use(compression());
 
 
 require("./routes/routes.js")(app);
